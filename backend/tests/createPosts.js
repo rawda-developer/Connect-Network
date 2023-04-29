96c9b25037e9a2de406cda5d8367ff2009870fff
Rawda Yasser <rawda.developer@gmail.com>
2023-04-29 17:23:14 +0300

    Feat: add createPosts functions to create dummy post data

---
+++ backend/tests/createPosts.js
@@ -0,0 +1,20 @@
+const Post = require("../src/models/post.model");
+
+let post1, post2;
+const createPost1 = async (user) => {
+  post1 = new Post({
+    text: "Hello world",
+    owner: user._id,
+  });
+  await post1.save();
+  return post1;
+};
+const createPost2 = async (user) => {
+  post2 = new Post({
+    text: "Hello world Post 2",
+    owner: user._id,
+  });
+  await post2.save();
+  return post2;
+};
+module.exports = { createPost1, createPost2 };
